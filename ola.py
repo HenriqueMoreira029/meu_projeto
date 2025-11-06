795d4a48ac041c0a20a34a049a86b72b54e51798
Akiyla <aylinhaprocznki@gmail.com>
2025-11-06 14:06:52 -0300

    dando push no arquivo certo

--- ola.py
+++ ola.py
@@ -1,1 +1,4 @@
 "print("Ola do Github")" 
+"print("hallo! :D im emu otori! emu meaaaanssss smiiileeeee :D")"
+"print("hallo da aylinha :D")"
+"print("não abrir esse link:https://www.youtube.com/watch?v=dQw4w9WgXcQ")"
